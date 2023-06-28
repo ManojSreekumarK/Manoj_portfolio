398a8f9299796b22918791d05caff36e2aae6751
ManojSreekumarK <manojsreekumar2000@gmail.com>
2023-06-28 12:44:58 +0530

    change in design logo

--- Portfolio/src/components/designcomponent/LogoDesign.vue
+++ Portfolio/src/components/designcomponent/LogoDesign.vue
@@ -45,50 +45,50 @@ $accentColor: #c778dd;
   background: $accentColor;
   position: relative;
   &:nth-child(1) {
-    width: 52px;
+    width: 51px;
   }
   &:nth-child(2) {
-    width: 42px;
-    top: 14px;
+    width: 41px;
+    top: 13.6px;
     left: -6px;
     transform: rotate(45deg);
   }
   &:nth-child(3) {
-    width: 42px;
-    top: 42px;
+    width: 41px;
+    top: 41px;
     left: -6px;
     transform: rotate(-45deg);
   }
   &:nth-child(4) {
-    top: 56px;
+    top: 54.5px;
     width: 52px;
   }
   &:nth-child(5) {
-    width: 52px;
-    top: 25px;
+    width: 51px;
+    top: 24.5px;
     left: 29px;
   }
   &:nth-child(6) {
-    width: 42px;
+    width: 43px;
     top: -20px;
     left: 45px;
     transform: rotate(-45deg);
   }
   &:nth-child(7) {
-    width: 41.5px;
+    width: 40.5px;
     top: 8px;
     left: 45px;
     transform: rotate(45deg);
   }
   &:nth-child(8) {
-    top: -37px;
+    top: -37.5px;
     left: 30px;
     width: 52px;
   }
   span {
     position: absolute;
     display: block;
-    height: 2px;
+    height: 1px;
     width: 5px;
     box-shadow: $accentColor 0 0 5px;
     animation: animate 2s infinite ease-in-out;
